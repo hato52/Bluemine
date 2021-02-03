2b60ffda564dae853a938ae277838085e77d524a
hato <hato.deve@gmail.com>
2021-02-04 00:33:08 +0900

    [update] home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :is_user_logged_in
   
   def index
+    @books = Book.limit(5).order('updated_at DESC')
+    @knowledges = Knowledge.limit(5).order('updated_at DESC')
   end
 end
